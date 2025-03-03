d3ed1e47de631761cda0b34d130143cc32aef3a2
Danny Armijos <daniel2667@hotmail.es>
2025-03-03 17:02:46 +0100

    Agregar lógica para generar el slug del producto antes de la inserción

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -43,4 +43,15 @@ export class Product {
     type: 'text',
   })
   gender: string;
+
+  @BeforeInsert()
+  CheckslugInsert() {
+    if (!this.slug) {
+      this.slug = this.title;
+    }
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '');
+  }
 }
