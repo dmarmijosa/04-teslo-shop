f9fdffda7a24a851f9aed3ca500e6e0ff62a8d61
Danny Armijos <daniel2667@hotmail.es>
2025-03-03 16:09:58 +0100

    Agregar módulo de productos con DTOs, entidad y controlador en NestJS

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(1)
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(1)
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
