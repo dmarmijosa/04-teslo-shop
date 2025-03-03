9dc9f3cf6f1963fab952c74298766060adb0c289
Danny Armijos <daniel2667@hotmail.es>
2025-03-03 16:21:37 +0100

    Modificar tipo de columna de precio a float y mejorar el método de creación de productos en el servicio

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRespository: Repository<Product>,
+  ) {}
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const product = this.productRespository.create(createProductDto);
+      await this.productRespository.save(product);
+      return product;
+    } catch (error) {
+      console.log(error);
+      throw new Error('Error creating product');
+    }
   }
 
   findAll() {
