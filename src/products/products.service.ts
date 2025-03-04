a1c3c6a5daf84eb4201c3949dc381b8a6f3a6e26
Danny Armijos <daniel2667@hotmail.es>
2025-03-04 04:29:39 +0100

    Agregar soporte para gestionar imágenes de productos en el DTO y entidad, y actualizar la lógica de creación en el servicio de productos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,7 +10,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Product } from './entities/product.entity';
+import { Product, ProductImage } from './entities';
 import { PaginationDTO } from 'src/common/dtos/pagination.dto';
 import { validate as isUUID } from 'uuid';
 
@@ -20,12 +20,20 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRespository: Repository<Product>,
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
   ) {}
   async create(createProductDto: CreateProductDto) {
     try {
-      const product = this.productRespository.create(createProductDto);
+      const { images = [], ...productDetails } = createProductDto;
+      const product = this.productRespository.create({
+        ...productDetails,
+        images: images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        ),
+      });
       await this.productRespository.save(product);
-      return product;
+      return { ...product, images };
     } catch (error) {
       this.handleDBExeptions(error);
     }
@@ -62,6 +70,7 @@ export class ProductsService {
     const product = await this.productRespository.preload({
       id: id,
       ...updateProductDto,
+      images: [],
     });
     if (!product)
       throw new NotFoundException(`Product with id: ${id} not found `);
