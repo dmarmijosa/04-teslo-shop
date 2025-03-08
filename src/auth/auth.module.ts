3f9ce8631cfb9d338f8f83e6709321fed528a453
Danny Armijos <daniel2667@hotmail.es>
2025-03-08 23:54:27 +0100

    Agregar soporte para autenticación JWT: actualizar módulo de autenticación y dependencias necesarias PASSORT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    // JwtModule.register({
+    //   secret: process.env.SECRET_PASSWORD,
+    //   signOptions: {
+    //     expiresIn: '2h',
+    //   },
+    // }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
   exports: [TypeOrmModule],
