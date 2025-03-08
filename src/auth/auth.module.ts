094bff973bb02bacffbf939041c010060aee9f03
Danny Armijos <daniel2667@hotmail.es>
2025-03-09 00:06:32 +0100

    Agregar validación de configuración en JwtModule: lanzar excepción si falta el secreto

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { InternalServerErrorException, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -13,11 +14,15 @@ import { JwtModule } from '@nestjs/jwt';
       defaultStrategy: 'jwt',
     }),
     JwtModule.registerAsync({
-      imports: [],
-      inject: [],
-      useFactory: () => {
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        if (!configService.get('SECRET_PASSWORD'))
+          throw new InternalServerErrorException(
+            'Error en la configuracion global',
+          );
         return {
-          secret: process.env.SECRET_PASSWORD,
+          secret: configService.get('SECRET_PASSWORD'),
           signOptions: {
             expiresIn: '2h',
           },
