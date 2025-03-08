6514e857ab41165e07ae59c63a86ffd45ac5489d
Danny Armijos <daniel2667@hotmail.es>
2025-03-08 22:19:16 +0100

    Agregar soporte para encriptar contraseñas en el servicio de autenticación y actualizar dependencias de bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
   Injectable,
   BadRequestException,
@@ -7,6 +11,7 @@ import { CreateUserDTO } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -16,9 +21,14 @@ export class AuthService {
   ) {}
   async create(createUserDto: CreateUserDTO) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
       await this.userRepository.save(user);
-      return user;
+      const { password: _, ...userWithoutPassword } = user;
+      return userWithoutPassword;
     } catch (error) {
       this.handleErrorDB(error);
     }
