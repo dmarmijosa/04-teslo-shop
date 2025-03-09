15277d801fd7400134c596f91cad5121cae77fdb
Danny Armijos <daniel2667@hotmail.es>
2025-03-09 18:57:48 +0100

    Agregar decorador RawHeader para obtener los encabezados crudos de la solicitud

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  Headers,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser, RawHeader } from './decorators';
+import { User } from './entities/user.entity';
+import { IncomingHttpHeaders } from 'http';
 
 @Controller('auth')
 export class AuthController {
@@ -19,10 +30,19 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  textingPrivateRoute() {
+  textingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeader() rawHeader: string[],
+    @Headers() headers: IncomingHttpHeaders,
+  ) {
     return {
       ok: true,
       message: 'Hola mundo',
+      user,
+      userEmail,
+      headers,
     };
   }
 }
---
+++ src/auth/decorators/raw-header.decorator.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const RawHeader = createParamDecorator((data, ctx: ExecutionContext) => {
+  const req = ctx.switchToHttp().getRequest();
+  return req.rawHeaders;
+});
