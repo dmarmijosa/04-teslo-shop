ab8363a3a089492c01836fc308093ad4cdf4bdac
Danny Armijos <daniel2667@hotmail.es>
2025-03-10 02:02:24 +0100

    Agregar protección de rutas en ProductsController y SeedModule con decorador Auth para roles de administrador

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,11 +4,16 @@ import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product, ProductImage } from './entities';
 import { CommonModule } from 'src/common/common.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product, ProductImage]), CommonModule],
+  imports: [
+    TypeOrmModule.forFeature([Product, ProductImage]),
+    CommonModule,
+    AuthModule,
+  ],
   exports: [ProductsService],
 })
 export class ProductsModule {}
--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -6,6 +6,7 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
+  // @Auth(ValidRoles.admin)
   executedSeed() {
     return this.seedService.runSeed();
   }
