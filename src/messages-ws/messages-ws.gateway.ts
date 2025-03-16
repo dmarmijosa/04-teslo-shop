74b06c681d8878feec5cbaacc82b147add2d5ccc
Danny Armijos <daniel2667@hotmail.es>
2025-03-16 21:30:04 +0100

    Agregar autenticación de usuario en WebSocket y mejorar el manejo de clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   WebSocketGateway,
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketServer,
   SubscribeMessage,
+  WsException,
 } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 import { Socket, Server } from 'socket.io';
 import { NewMessageDto } from './dto/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
@@ -15,11 +19,21 @@ export class MessagesWsGateway
 {
   @WebSocketServer() wss: Server;
 
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  handleConnection(client: Socket) {
-    console.log(client.handshake.headers.authentication);
-    this.messagesWsService.registerClient(client);
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayload;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
     this.wss.emit(
       'clients-updated',
       this.messagesWsService.getConnectedClientes(),
@@ -46,7 +60,7 @@ export class MessagesWsGateway
     // });
 
     this.wss.emit('message-from-server', {
-      fullName: 'YO',
+      fullName: this.messagesWsService.getUserFullNameBySocketId(cliente.id),
       message: payload.message || 'no message !!',
     });
   }
