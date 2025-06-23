b7738dd4abab2492f689562f1ccad4e7325ca33a
Danny Armijos <daniel2667@hotmail.es>
2025-06-23 21:03:06 +0200

    Actualizar configuración de CORS para permitir solicitudes solo desde el dominio específico

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*', // O especifica tu dominio frontend
+    origin: 'https://teslo-shop-danny.netlify.app', // O especifica tu dominio frontend
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
